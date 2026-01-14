131448c1ba88ef7a44cec1525707c7fe438144c2
Trvia4d2 <legokraken11@gmail.com>
2026-01-14 17:37:36 -0500

    character update

--- RPG Project/character.h
+++ RPG Project/character.h
@@ -1,7 +1,14 @@
 #pragma once
 class character
 {
-	int HP;
-};
+public:
+	int HP = 3;
+	int T = 2;
+	int Move = 3;
+	int dex = 4;
+	int str = 4;
+	int wits = 4;
+	int XP = 3000;
+	int gold = 100;
 
-//test
+};
