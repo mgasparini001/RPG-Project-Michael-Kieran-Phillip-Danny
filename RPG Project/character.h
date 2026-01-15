99d2ea5e996da57a2e6e39f2796006dc015f4347
Kieran Kelly <kieranjokelly@gmail.com>
2026-01-15 16:33:39 -0500

    refactor: simplify constructor declaration in Character class

--- RPG Project/character.h
+++ RPG Project/character.h
@@ -16,20 +16,18 @@ protected:
     std::string name;
 
 public:
-    Character(const std::string& name,
-              int hp,
-              int move,
-              int dex,
-              int str,
-              int wits,
-              int toughness,
-              int dmg);
+    Character(const std::string& name, int hp, int move, int dex, int str, int wits, int toughness, int dmg);
 
     virtual ~Character() = default;
 
     virtual void attack(Character& target);
+
     void takeDamage(int amount);
+
     bool isAlive() const;
+
     int getHp() const;
+
     std::string getName() const;
+	
 };
