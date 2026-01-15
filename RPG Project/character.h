df6ce85ce39704392d2cba12de08b1ad0ba5078e
Trvia4d2 <legokraken11@gmail.com>
2026-01-15 15:51:03 -0500

    update

--- RPG Project/character.h
+++ RPG Project/character.h
@@ -1,14 +1,16 @@
 #pragma once
+#include "npc.h"
 class character
 {
 public:
-	int HP = 3;
-	int T = 2;
-	int Move = 3;
-	int dex = 4;
-	int str = 4;
-	int wits = 4;
-	int XP = 3000;
-	int gold = 100;
+	int HP;
+	int T;
+	int Move;
+	int Dex;
+	int Str;
+	int Wits;
+	int Dmg;
+
+	void attack(){}
 
 };
--- RPG Project/player.h
+++ RPG Project/player.h
@@ -1,7 +1,15 @@
 #pragma once
 #include "character.h"
-class player :
-    public character
+class player :public character
 {
+	class character
+	{
+	public:
+		int XP = 3000;
+		int gold = 100;
+
+		void attack() {}
+
+	};
 };
 
