9f2890219569866eba463f758e215d679cd7a5ff
Trvia4d2 <legokraken11@gmail.com>
2026-01-22 17:01:40 -0500

    arsen

--- RPG Project/character.cpp
+++ RPG Project/character.cpp
@@ -4,7 +4,7 @@ using std::string;
 
 Character::Character(const string& name, int hp, int move, int dex, int str, int wits, int toughness, int dmg) : name(name), hp(hp), maxHp(hp), move(move), dex(dex), str(str), wits(wits), toughness(toughness), dmg(dmg) {}
 
-void Character::attack(Character& target){
+void Character::attack(Character& target) {
     target.takeDamage(dmg);
 }
 
