29a5a52fa13164bd044a9d15dda86b368b4695c9
mgasparini001 <mrsplashman9000@gmail.com>
2026-01-29 18:00:44 -0500

    small changes to object instantiations

--- RPG Project/heavy.h
+++ RPG Project/heavy.h
@@ -3,6 +3,7 @@
 class heavy :
     public enemy
 {
+
     heavy(const std::string& name, int hp, int melee, int range, int armor, int stamina, int dmg, int ap);
     
     void attackMessage(Character& target) override;
