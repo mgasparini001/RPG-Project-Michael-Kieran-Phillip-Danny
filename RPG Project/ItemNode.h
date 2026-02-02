b8fa171e10ed2b7ab03e4060df15526cd6c00c4e
Daniel Patterson <starwarsdanny77@gmail.com>
2026-02-02 16:24:41 -0500

    Added header files for the DLL and item node classes.

---
+++ RPG Project/ItemNode.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+
+template<class T>
+class itemNode
+{
+private:
+	T item;
+	itemNode* prevItem;
+	itemNode* nextItem;
+public:
+	itemNode(T i);
+	T getItem();
+	void setPrev(T prevItem);
+	void setNext(T setNext);
+	itemNode* getNext();
+	itemNode getPrev();
+};
