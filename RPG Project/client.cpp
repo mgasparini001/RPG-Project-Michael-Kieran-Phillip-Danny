b933f585f48bc58ff502581a1a9445215343d3ff
mgasparini001 <mrsplashman9000@gmail.com>
2026-02-06 18:06:18 -0500

    equip item, menu tweaks

--- RPG Project/client.cpp
+++ RPG Project/client.cpp
@@ -218,7 +218,7 @@ void refreshScreen() {
 // Input: an enemy and a player    Output: an interactive battle loop that reverts to main menu upon ending
 // desc: displays enemy name and hp, and starts battle sequence loop, letting user either fight, rest, or flee. uses Character hp, stamina, dmg, and armor penetration (strength)
 // lets player decide on melee or ranged attack, with ranged having a chance to miss but taking less stamina
-void enterBattle(enemy& enemy, player& p1) {
+void enterBattle(enemy& enemy, player& p1, ItemRegistry registry) {
 	sf::Music Music;
 	bool hasRun = false;
 
